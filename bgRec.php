07c25e76610bf0a36495974fac046e462b61f2ae
nBGUgger <109363235+nBGUgger@users.noreply.github.com>
2022-07-15 22:03:13 +0530

    Update and rename README.md to bgRec.php

---
+++ bgRec.php
@@ -0,0 +1,10 @@
+<!-- For prac for bg hunter -->
+<?php
+$username = $_POST['username'];
+$password = $_POST['userPassword'];
+
+if(isset(username) && !empty(username)){
+  echo "the username is "+$username+"and the password is "+$password;
+  
+};
+?>
